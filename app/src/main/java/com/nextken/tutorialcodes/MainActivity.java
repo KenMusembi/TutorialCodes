83f2bc737a43af66a6cd94f24f8c61074d232721
KenMusembi <kenmusembi21@gmail.com>
2021-05-09 13:35:32 +0300

    implimenting Toast, Variable declaration

--- app/src/main/java/com/nextken/tutorialcodes/MainActivity.java
+++ app/src/main/java/com/nextken/tutorialcodes/MainActivity.java
@@ -7,26 +7,34 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private TextView txtHello;
+    private EditText editTxtName;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public void onBtnClick(View view) {
-        TextView fnameView = (TextView)findViewById(R.id.firstNameView);
-        TextView snameView = (TextView)findViewById(R.id.secondNameView);
-        TextView emailView = (TextView)findViewById(R.id.emailView);
 
-        EditText fnameEdit = (EditText)findViewById(R.id.firstNameEdit);
-        EditText snameEdit = (EditText)findViewById(R.id.secondNameEdit);
-        EditText emailEdit = (EditText)findViewById(R.id.emailEdit);
-
-        fnameView.setText("First Name: " +fnameEdit.getText().toString());
-        snameView.setText("Second Name: "+snameEdit.getText().toString());
-        emailView.setText("Email: "+emailEdit.getText().toString());
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(this);
+        txtHello = (TextView)findViewById(R.id.txtHello);
+        editTxtName = (EditText)findViewById(R.id.editTxtName);
     }
 
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button:
+                Toast.makeText(MainActivity.this, "Hello Button Pressed", Toast.LENGTH_LONG).show();
+                txtHello.setText(" Hello " + editTxtName.getText().toString());
+                break;
+            default:
+                break;
+        }
+    }
 }
